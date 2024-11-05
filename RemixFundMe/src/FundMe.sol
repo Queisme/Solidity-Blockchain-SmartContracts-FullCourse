e451db80a6f5d711781da115b12b97c233886b13
Queisme <queiskey@gmail.com>
2024-11-05 03:29:27 -0600

    Update

--- RemixFundMe/src/FundMe.sol
+++ RemixFundMe/src/FundMe.sol
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+//import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
 
@@ -20,14 +20,12 @@ contract FundMe {
     function getPrice() public {
         // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
         // ABI
-
-        AggregatorV3Interface priceFeed 
     }
 
     function getConversionRate() public {}
 
     function getVersion() public view returns(uint256){
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+        //return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 
 
